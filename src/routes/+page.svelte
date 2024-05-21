3cbb6434233e0e67e17a82f7866de01a8cb0b1cb
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-05-21 23:00:07 +0800

    Init files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<div class="">
+	<section class="h-screen flex flex-col justify-center items-center">
+		<div class="">Landing goes here</div>
+	</section>
+	<section class="min-h-screen">
+		<h1 class="bg-black text-white w-full sticky top-12 text-4xl">Section 1</h1>
+		<div class="">Hello</div>
+	</section>
+	<section>
+		<h1 class="bg-red-500 text-white w-full sticky top-12 text-4xl">Section 2</h1>
+		<div class="h-screen">Hello</div>
+		<h1 class="bg-black text-white w-full sticky top-12 text-4xl">Projects</h1>
+		<div class="h-screen">Hello</div>
+	</section>
+</div>

686b8658a5ee7950db5cc8af843a0e52d4011bc1
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-02-19 19:05:58 +0800

    Create error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+Error 404 This page does not exist
+<p>You are here, because you either:</p>
+<ul>
+	<li>visited a non-existing link</li>
+	<li>mistyped the URL; or...</li>
+	<li>intentionally visited this page</li>
+</ul>
+<!--secret game starts here-->

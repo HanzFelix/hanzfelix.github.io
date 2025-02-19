92e609076ce107c2a418afd0f6f09b8502dd8efa
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-02-19 19:10:08 +0800

    Fix line break

--- src/routes/Hello.svelte
+++ src/routes/Hello.svelte
@@ -7,10 +7,10 @@
 		class="container mx-auto flex h-full flex-col-reverse items-stretch justify-between gap-8 border-2 border-black px-8 py-12 md:flex-row md:items-center"
 	>
 		<div class="">
-			<h1 class="mb-2 flex text-5xl font-black md:text-6xl">
+			<h1 class="mb-2 flex flex-wrap text-5xl font-black md:text-6xl">
 				I'm&nbsp;
 				<span
-					class="rotate-180 transform whitespace-nowrap text-center"
+					class="rotate-180 transform select-none whitespace-nowrap text-center"
 					style="writing-mode: horizontal-tb;"
 				>
 					XIℸƎℲ ZNVH

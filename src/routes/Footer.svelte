aeea6535f0559df4936ad2cc1add7c6acd175a4d
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-09-07 20:21:29 +0800

    feat: add text to footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,1 +1,3 @@
-<section class="">Footer</section>
+<section class=" border-gray-700 border-b-8 text-gray-500 text-sm">
+	<div class="container mx-auto px-8 py-2">&copy;2025 Hanz Felix Tumulak</div>
+</section>

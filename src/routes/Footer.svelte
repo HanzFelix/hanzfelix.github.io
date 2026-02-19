385a142ae70a0ec3a107c75e5ac9215accc2f7a3
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2026-02-19 08:23:28 +0800

    chore: update year

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,3 @@
 <section class=" border-gray-700 border-b-8 text-gray-500 text-sm">
-	<div class="container mx-auto px-8 py-2">&copy;2025 Hanz Felix Tumulak</div>
+	<div class="container mx-auto px-8 py-2">&copy;2026 Hanz Felix Tumulak</div>
 </section>
